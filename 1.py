6cd2978a07d200aa01d85640281af48831361f16
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-07-25 10:16:29 +0700

    Variables

---
+++ 1.py
@@ -0,0 +1,10 @@
+a = 2
+b = 10
+my_variable = 97
+any_variable_name = 100
+
+string_variable = "hello world!"
+single_quotes = 'String can have single quotes'
+
+print(string_variable)
+print(my_variable)
