389b27a395c68996d075cedce886872cd6302139
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-07-26 18:09:31 +0700

    If Statements

--- 5.py
+++ 5.py
@@ -5,3 +5,22 @@ if person in known_people:
   print('You know {}!'.format(person))
 else:
   print('You dont {}!'.format(person))
+
+## for example case
+
+def who_do_you_know():
+  people = input('Enter the names of people you know, separated by commas: ')
+  people_list = people.split(',')
+  
+  people_without_space = []
+  for person in people_list:
+    people_without_space.append(person.strip())
+  
+  return people_without_space
+
+def ask_user():
+  person = input('Enter a name of someone you know: ')
+  if person in who_do_you_know():
+    print('You know {}!'.format(person))
+
+ask_user()
