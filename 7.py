aec2614c1a889ce565a55a0db685758abd236545
Ari Bambang Kurniawan <aribk210@gmail.com>
2019-07-26 19:07:54 +0700

    Dictionaries

---
+++ 7.py
@@ -0,0 +1,30 @@
+my_set = {1, 3, 5}
+my_dict = {'name': 'Ari', 'age': 21, 'grades': [13, 15, 17]}
+another_dict = {1: 10, 2: 20, 3: 30}
+
+lottery_player = [{
+    'name': 'bambang',
+    'numbers': (10, 25, 33, 40)
+  },{
+    'name': 'bambang',
+    'numbers': (10, 25, 33, 40)
+  }
+]
+universities = [
+  {
+    'name': 'Institut Teknologi Sumatera',
+    'location': 'Lampung'
+  },
+  {
+    'name': 'Institut Teknologi Bandung',
+    'location': 'Bandung'
+  }
+]
+
+#sum(lottery_player['numbers'])
+#lottery_player['name'] = 'ari'
+
+##
+
+#lottery_player[0].total()
+#lottery_player.total()
